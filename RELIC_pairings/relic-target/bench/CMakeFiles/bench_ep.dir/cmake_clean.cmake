180cfaf037f8763227f65d161428134d20c810e3
primefieldsfp <99787213+primefieldsfp@users.noreply.github.com>
2022-02-16 00:36:07 -0800

    RELIC commit

---
+++ RELIC_pairings/relic-target/bench/CMakeFiles/bench_ep.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/bench_ep"
+  "../bin/bench_ep.pdb"
+  "CMakeFiles/bench_ep.dir/bench_ep.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bench_ep.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
