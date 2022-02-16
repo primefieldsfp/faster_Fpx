3da6e1d7986517a855701de0ff09d3da4e78a9a9
primefieldsfp <99787213+primefieldsfp@users.noreply.github.com>
2022-02-16 00:09:14 -0800

    RELIC commit

---
+++ RELIC_pairings/relic/bench/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/plonga/Documents/pairings/relic/bench
+# Build directory: /home/plonga/Documents/pairings/relic/bench
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
