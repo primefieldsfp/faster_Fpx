82aee3d0dcc0732980c7a190e60d7250cd8272a9
primefieldsfp <99787213+primefieldsfp@users.noreply.github.com>
2022-02-16 00:39:08 -0800

    RELIC commit

---
+++ RELIC_pairings/relic-target/bench/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/plonga/Documents/Faster_Fp_arithmetic/GitHub_faster_Fp_arithmetic/Pairings/relic/bench
+# Build directory: /home/plonga/Documents/Faster_Fp_arithmetic/relic-target/bench
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
