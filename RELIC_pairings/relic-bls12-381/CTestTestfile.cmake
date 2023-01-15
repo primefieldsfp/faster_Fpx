ab46e68b05b6f9b5936be8143474653f0263f33e
primefieldsfp <99787213+primefieldsfp@users.noreply.github.com>
2023-01-15 09:20:14 -0800

    Add files via upload

---
+++ RELIC_pairings/relic-bls12-381/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/plonga/Documents/pairings/relic
+# Build directory: /home/plonga/Documents/pairings/relic
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("test")
+subdirs("bench")
