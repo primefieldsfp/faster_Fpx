9ff032a34039a74698aa721f0ae9ff275f97f0a0
primefieldsfp <99787213+primefieldsfp@users.noreply.github.com>
2023-01-15 09:49:04 -0800

    Add files via upload

---
+++ RELIC_pairings/relic-bls12-381/test/CMakeFiles/test_fpx.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/test_fpx"
+  "../bin/test_fpx.pdb"
+  "CMakeFiles/test_fpx.dir/test_fpx.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_fpx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
