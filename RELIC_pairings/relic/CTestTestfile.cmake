6140492a4eb01c27cac0a8d00a0080f15472393c
primefieldsfp <99787213+primefieldsfp@users.noreply.github.com>
2023-01-15 09:09:48 -0800

    Delete RELIC_pairings/relic directory

--- RELIC_pairings/relic/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/plonga/Documents/pairings/relic
-# Build directory: /home/plonga/Documents/pairings/relic
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
-subdirs("bench")
