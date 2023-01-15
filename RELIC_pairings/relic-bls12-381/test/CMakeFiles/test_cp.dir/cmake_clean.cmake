ea5c082ffa7e4e172669a09f3affac9ed4fe5756
primefieldsfp <99787213+primefieldsfp@users.noreply.github.com>
2023-01-15 09:46:47 -0800

    Add files via upload

---
+++ RELIC_pairings/relic-bls12-381/test/CMakeFiles/test_cp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/test_cp"
+  "../bin/test_cp.pdb"
+  "CMakeFiles/test_cp.dir/test_cp.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_cp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ RELIC_pairings/relic-bls12-381/test/CMakeFiles/test_dv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/test_dv"
+  "../bin/test_dv.pdb"
+  "CMakeFiles/test_dv.dir/test_dv.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_dv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
